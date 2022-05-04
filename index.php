8ef19a6e613fe30add74190281b033580759168a
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-04 16:12:08 +0200

    header (fiexd) complete

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. 
+*/
+$domandaRisposta = array(
+    array("Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?", ["La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.", "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.", "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.", "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge."]),
+
+);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0510089784.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="title.ico" />
+    <link rel="stylesheet" href="style.css">
+    <title>Domande frequenti- Privacy e termini</title>
+
+</head>
+
+<body>
+    <header class="container-fluid position-fixed bg-white">
+        <div class="row">
+            <div class="col-6 py-4">
+                <div>
+                    <img src="Google_2015_logo.svg.png" alt="GoogleLogo">
+                    <h4 class="c-grey d-inline">Privacy e termini</h4>
+                </div>
+            </div>
+            <div class="col-6 d-flex py-4 justify-content-end">
+                <div>
+                    <i class="fas fa-bars px-4 pt-3"></i>
+                </div>
+                <div>
+                    <img src="user.svg" alt="user">
+                </div>
+            </div>
+            <div class="col-12">
+                <ul class="d-flex c-grey ">
+                    <li>
+                        <a href="">Introduzione</a>
+                    </li>
+                    <li class="px-4">
+                        <a href="privacy">Norme sulla privacy</a>
+                    </li>
+                    <li>
+                        <a href="terms">Termini di servizio</a>
+                    </li>
+                    <li class="px-4">
+                        <a href="technologies">Tecnologie</a>
+                    </li>
+                    <li>
+                        <a>Domande frequenti</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </header>
+    <main>
+        <div class="container">
+            <?php
+            for ($i = 0; $i < count($domandaRisposta); $i++) {
+                echo ("<h1>" . $arrayAlunni[$i][0] . "<h1>");
+
+                for ($j = 0; $j < count($domandaRisposta[$i][1]); $j++) {
+                    echo ("<p>" . $domandaRisposta[$i][1] . "</p>");
+                    echo ("<p>");
+                }
+                echo ("<p>media: " . $media . "<p>");
+            }
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
